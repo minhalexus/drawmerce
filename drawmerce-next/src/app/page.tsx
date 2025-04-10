742013ce3bc42c1483ed65f6101a5d45b7158cfc
Minhal Shanjer <mshanjer@gmail.com>
2025-04-10 18:09:39 -0400

    add a line to the polygon images

--- drawmerce-next/src/app/page.tsx
+++ drawmerce-next/src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
 
         <div className="w-screen h-screen relative overflow-hidden">
             {/* Diagonal Split */}
-            <div className="absolute inset-0" style={{clipPath: "polygon(0 0, 70% 0, 45% 100%, 0 100%)"}}>
+            <div className="absolute inset-0 border-[0.7rem] border-black"
+                 style={{clipPath: "polygon(0 0, 70% 0, 45% 100%, 0 100%)"}}>
                 <Image
                     src="/jahesh.jpg"
                     alt="Larger Portion Image"
@@ -15,7 +16,20 @@ export default function Home() {
                     objectFit="cover"
                 />
             </div>
-            <div className="absolute inset-0" style={{clipPath: "polygon(70% 0, 100% 0, 100% 100%, 45% 100%)"}}>
+
+            {/* Black Line Along Polygon Edge */}
+            <div className="absolute bg-black"
+                 style={{
+                     clipPath: "polygon(70% 0, 70.5% 0, 45.5% 100%, 45% 100%)",
+                     width: "100%",
+                     height: '100vh',
+                     zIndex: 4
+                 }}>
+            </div>
+
+
+            <div className="absolute inset-0 border-[0.7rem] border-black"
+                 style={{clipPath: "polygon(70% 0, 100% 0, 100% 100%, 45% 100%)"}}>
                 <Image
                     src="/stefan.jpg"
                     alt="Smaller Portion Image"
