65944c5047a62a3d4a190e7aef12a1b17c790930
Minhal Shanjer <mshanjer@gmail.com>
2025-04-10 17:33:34 -0400

    added images and split it in display flex.

--- drawmerce-next/next.config.ts
+++ drawmerce-next/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+
+import {ImageConfig} from "next/dist/server/image-config";
+
+/** @type {ImageConfig} Allow images sourced from the correct paths **/
+const images = {
+    domains: ["www.pinterest.com", "www.freepik.com"],
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
+    // devIndicators: false
+    images,
 };
+
 
 export default nextConfig;
